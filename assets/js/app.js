fcc7700dd27995a52807c11c5fcf822a15f708a3
amira ashour <93158924as@gmail.com>
2024-12-02 01:09:07 +0200

    add cards to bike card

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,85 +1,85 @@
 async function sendHttpsRequest(data) {
-  try {
-    const response = await axios.post(url, data);
-    console.log(response);
-  } catch (error) {
-    console.log('Error:', error.message);
-  }
+    try {
+        const response = await axios.post(url, data);
+        console.log(response);
+    } catch (error) {
+        console.log('Error:', error.message);
+    }
 }
 
 // Show input error message
 function showError(input, message) {
-  const formControl = input.parentElement;
-  if (formControl.className.includes('full'))
-    formControl.className = 'input-container input-full error';
-  else formControl.className = 'input-container error';
-  const small = formControl.querySelector('small');
-  small.innerText = message;
+    const formControl = input.parentElement;
+    if (formControl.className.includes('full'))
+        formControl.className = 'input-container input-full error';
+    else formControl.className = 'input-container error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 }
 
 // Show success outline
 function showSuccess(input) {
-  const formControl = input.parentElement;
-  if (formControl.className.includes('full'))
-    formControl.className = 'input-container success input-full';
-  else formControl.className = 'input-container success';
+    const formControl = input.parentElement;
+    if (formControl.className.includes('full'))
+        formControl.className = 'input-container success input-full';
+    else formControl.className = 'input-container success';
 }
 
 // Check email is valid
 function checkEmail(input) {
-  const re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  // console.log(input.value, re.test(input.value.trim()));
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, 'Email is not valid');
-  }
+    const re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // console.log(input.value, re.test(input.value.trim()));
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
 }
 
 // Check required fields
 function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
-      return false;
-    } else {
-      showSuccess(input);
-    }
-  });
-  return true;
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+            return false;
+        } else {
+            showSuccess(input);
+        }
+    });
+    return true;
 }
 
 // Check input length
 function checkLength(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters`
-    );
-    return false;
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters`
-    );
-    return false;
-  } else {
-    showSuccess(input);
-    return true;
-  }
+    if (input.value.length < min) {
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        );
+        return false;
+    } else if (input.value.length > max) {
+        showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+        );
+        return false;
+    } else {
+        showSuccess(input);
+        return true;
+    }
 }
 
 // Check passwords match
 function checkPasswordsMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, 'Passwords do not match');
-    return false;
-  }
-  return true;
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+        return false;
+    }
+    return true;
 }
 
 // Get fieldname
 function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
