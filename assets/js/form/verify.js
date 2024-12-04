a86abdc5c16d5f4111340f82919379a1d6ff2612
ibrahim-hassan74 <ih3046376@gmail.com>
2024-12-04 21:17:48 +0200

    solve problem of conflict event listener

--- assets/js/form/verify.js
+++ assets/js/form/verify.js
@@ -1,14 +1,13 @@
 const formLogin = document.querySelector('.login-form');
 let cnt = 0;
 let userId;
-formLogin.addEventListener('submit', async function (event) {
+async function handleSubmit(event) {
   event.preventDefault();
   if (cnt === 0) {
     const code = document.getElementById('code');
     const data = {
       OTP: code.value,
     };
-    // const formData = new FormData(formLogin);
     try {
       const response = await axios.post(verifyUrl, data);
       console.log(response);
@@ -18,10 +17,8 @@ formLogin.addEventListener('submit', async function (event) {
         container.innerHTML = `
           <h1 class="heading-primary">Set a password</h1>
           <p class="login-description">
-            Your previous password has been reseted. Please set a new password for
-            your account.
+            Your previous password has been reset. Please set a new password for your account.
           </p>
-    
           <form class="login-form">
             <div class="input-container">
               <label for="newpassword">Create Password</label>
@@ -38,7 +35,10 @@ formLogin.addEventListener('submit', async function (event) {
             </div>
             <button type="submit" class="login-button">Set Password</button>
           </form>
-          `;
+        `;
+        document
+          .querySelector('.login-form')
+          .addEventListener('submit', handleSubmit);
         cnt = 1;
       }
     } catch (error) {
@@ -50,17 +50,18 @@ formLogin.addEventListener('submit', async function (event) {
     const data = {
       user_id: userId,
       password: newPassword.value,
-      confirm_password: confirmPassword.value,
+      password2: confirmPassword.value,
     };
     try {
       const response = await axios.post(setpassUrl, data);
       console.log(response);
       if (response.status === 200) {
-        // alert('Password set successfully!');
-        window.location.href = '/my-component/login.html';
+        alert('Password set successfully!');
+        // window.location.href = '/my-component/login.html';
       }
     } catch (error) {
       console.log('Error:', error.message);
     }
   }
-});
+}
+formLogin.addEventListener('submit', handleSubmit);
