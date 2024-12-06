0cd39fd5ec668cf86fbae06edd7e8390f12d2ab1
ibrahim-hassan74 <ih3046376@gmail.com>
2024-12-06 22:58:13 +0200

    add details for each item in my bike store

---
+++ assets/js/addCard.js
@@ -0,0 +1,26 @@
+cards.forEach((card) => {
+  card.addEventListener('click', (event) => {
+    const clickedCard = event.currentTarget;
+    const imageUrl = clickedCard.querySelector('.card-image img').src;
+    const cardTitle = clickedCard.querySelector('.card-title').innerHTML;
+    const cardPrice = clickedCard.querySelector('.card-price span').innerHTML;
+    const cardDescription =
+      clickedCard.querySelector('.card-description').innerHTML;
+    const cardText = clickedCard.querySelector('.card-text').innerHTML;
+
+    // console.log('Clicked Card Details:');
+    sessionStorage.setItem('imageUrl', imageUrl);
+    sessionStorage.setItem('cardTitle', cardTitle);
+    sessionStorage.setItem('cardPrice', cardPrice);
+    sessionStorage.setItem('cardDescription', cardDescription);
+    sessionStorage.setItem('cardText', cardText);
+    window.location.href = 'details.html';
+    // console.log('Image URL:', imageUrl);
+    // console.log('Card Title:', cardTitle);
+    // console.log('Card Price:', cardPrice);
+    // console.log('Card Description:', cardDescription);
+    // console.log('Card Text:', cardText);
+
+    // You can now use the extracted data as needed
+  });
+});
---
+++ assets/js/details.js
@@ -0,0 +1,31 @@
+function create() {
+  details.innerHTML = `
+        <div class="img-box">
+          <img src="${sessionStorage.getItem('imageUrl')}" alt="bike image" />
+        </div>
+        <div class="content">
+          <h3 class="name-bike">${sessionStorage.getItem('cardTitle')}</h3>
+          <h4 class="price-bike">${sessionStorage.getItem('cardPrice')}</h4>
+          <p class="degree-bike">${sessionStorage.getItem(
+            'cardDescription'
+          )}</p>
+          <div class="quantity-box">
+            <label for="quantity">Quantity:</label>
+            <input
+              type="number"
+              min="1"
+              max="1000"
+              value="1"
+              name="quantity"
+              id="quantity"
+            />
+          </div>
+          <a href="#" class="btn-cart">Add to cart</a>
+          <p class="description">
+            ${sessionStorage.getItem('cardText')}}
+          </p>
+        </div>
+    `;
+}
+
+create();
